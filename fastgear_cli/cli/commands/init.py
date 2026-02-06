4e93f5f688309d4d7fe446690f92e9f5020953f3
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2026-02-06 20:54:58 +0000

    🚚 Rename modules and update import paths to reflect new structure

--- fastgear_cli/cli/commands/init.py
+++ fastgear_cli/cli/commands/init.py
@@ -1,8 +1,8 @@
 import typer
 
-from fg.cli.prompts.project import ask_project_name
-from fg.core.filesystem import create_project
-from fg.core.models import ProjectInitConfig
+from fastgear_cli.cli.prompts.project import ask_project_name
+from fastgear_cli.core.filesystem import create_project
+from fastgear_cli.core.models import ProjectInitConfig
 
 init_app = typer.Typer(help="Project initialization command")
 
