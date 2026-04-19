db3efa7086ca672ffd04e9e3c07360c3e45a9b22
Henrique Marcuzzo <henrique.souza.m06@gmail.com>
2026-04-19 19:29:27 +0100

    ✨ Add version display functionality to CLI

--- fastgear_cli/cli/app.py
+++ fastgear_cli/cli/app.py
@@ -1,12 +1,27 @@
+from typing import Annotated, Optional
+
 import typer
 
+from fastgear_cli import __version__
 from fastgear_cli.cli.commands.add import add_app
 from fastgear_cli.cli.commands.init import init_app
 
-app = typer.Typer(help="Fastgear code generator")
+app = typer.Typer(help="Fastgear code generator", invoke_without_command=True)
 
 app.add_typer(init_app)
 app.add_typer(add_app)
+
+
+@app.callback()
+def main_callback(
+    version: Annotated[
+        Optional[bool],
+        typer.Option("--version", "-v", is_eager=True, help="Show version and exit"),
+    ] = None,
+) -> None:
+    if version:
+        typer.echo(f"fg v{__version__}")
+        raise typer.Exit()
 
 
 def main():
